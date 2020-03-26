62b319bbdd2f7e16d25ac62bd501dde5da55b53a
Isti16 <isti001116@gmail.com>
2020-03-26 14:16:22 +0100

    up

--- src/Content.ts
+++ src/Content.ts
@@ -17,7 +17,6 @@ export default class Content {
             fs.createReadStream("favicon.ico").pipe(res);
             return;
         }
-        
 
         // Weboldal inicializálása + head rész:
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
